7f2670a51424d705a32fd93d97f90c9eaabcb6bf
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-06 22:16:37 -0600

    refactor: create separate functions

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@ fn main() {
     // ["target/debug/minigrep"]
     let args: Vec<String> = env::args().collect::<Vec<String>>();
 
+    let (query, filename) = parse_config(&args);
+
+    let contents = read_contents_from_file(filename);
+}
+
+fn parse_config(args: &[String]) -> (&String, &String) {
     // the first argument is the path of the binary. By index 0 we will ignore it.
     let query: &String = &args[1];
     let filename: &String = &args[2];
@@ -14,7 +20,13 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+    (query, filename)
+}
 
+fn read_contents_from_file(filename: &String) -> String {
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    println!("With contents: {}", contents);
+
+    println!("With text:\n{}", contents);
+
+    contents
 }
