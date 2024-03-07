3e1fd02d58b16d9817c990fb59d9c621fab536c2
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-06 22:01:07 -0600

    feat: fetch query and filename from arguments

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@ use std::env;
 
 fn main() {
     // by default it will return the path of the binary
+    // ["target/debug/minigrep"]
     let args: Vec<String> = env::args().collect::<Vec<String>>();
+
+    // the first argument is the path of the binary. By index 0 we will ignore it.
+    let query: &String = &args[1];
+    let filename: &String = &args[2];
 
     println!("{:?}", args);
 }
