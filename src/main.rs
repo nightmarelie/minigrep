155bcb04b20c0c00d1e4f363bc099c4279110265
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-03-07 23:35:31 -0600

    refactor: restructuring main file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+use minigrep::{Config, run};
+
 
 fn main() {
     // by default, it will return the path of the binary
@@ -17,37 +17,4 @@ fn main() {
         println!("Application error: {}", e);
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        // the first argument is the path of the binary. By index 0 we will ignore it.
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        println!("{:?}", args);
-
-        println!("Searching for {}", query);
-        println!("In file {}", filename);
-
-        Ok(Config { query, filename })
-    }
-}
-
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
 }
